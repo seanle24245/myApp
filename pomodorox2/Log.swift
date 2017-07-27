8c7c36d2f6e2effac84355a82b08789f1318023c
seanle24245 <sean.le24245@gmail.com>
2017-07-27 18:31:23 +0200

    finalUI

---
+++ pomodorox2/Log.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  pomodorox2
+//
+//  Created by Sean Le on 7/27/17.
+//  Copyright © 2017 Sean Le. All rights reserved.
+//
+
+import Foundation
+
+var content:String
+var entryTime:Date
