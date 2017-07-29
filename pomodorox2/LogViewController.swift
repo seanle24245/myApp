d96ffe9fe665510d3729aba28e048dac52a5e250
seanle24245 <sean.le24245@gmail.com>
2017-07-29 20:26:02 +0200

    log view changes

--- pomodorox2/LogViewController.swift
+++ pomodorox2/LogViewController.swift
@@ -1,3 +1,13 @@
+
+//
+//  LogViewController.swift
+//  pomodorox2
+//
+//  Created by Sean Le on 7/29/17.
+//  Copyright © 2017 Sean Le. All rights reserved.
+//
+
+
 //
 //  focusView.swift
 //  pomodorox2
@@ -9,76 +19,79 @@
 import Foundation
 import UIKit
 import AudioToolbox
-class FocusViewController: UIViewController {
-
+class LogViewController: UIViewController {
+    
     var timer:Timer=Timer()
     var totalSeconds:Int=5
     var initialTime:Int=5
     var timerIsOn:Bool=false
     var minutesToDisplay:Int=0
     var secondsToDisplay:Int=0
-    @IBOutlet weak var focusTimeLabel: UILabel!
-    @IBOutlet weak var focusProgressBar: UIProgressView!
+    
+    
+    @IBOutlet weak var logTimeLabel: UILabel!
+    
+    @IBOutlet weak var logProgressBar: UIProgressView!
     
     
     
-    
-    @IBAction func startButton(_ sender: Any) {
-        
+    @IBAction func logStartButton(_ sender: Any) {
         if timerIsOn == false{
             
-        
-        
-        timer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(FocusViewController.updateTimer)), userInfo: nil, repeats: true)
+            
+            
+            timer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(FocusViewController.updateTimer)), userInfo: nil, repeats: true)
             timerIsOn=true
-        }
         
+    }
     }
     
     
-    @IBAction func pauseButton(_ sender: Any) {
+        
+    @IBAction func logPauseButton(_ sender: Any) {
         timer.invalidate()
         
-       
-        focusTimeLabel.text="\(minutesToDisplay)"+":"+"\(secondsToDisplay)"
+        
+        logTimeLabel.text="\(minutesToDisplay)"+":"+"\(secondsToDisplay)"
         timerIsOn=false
     }
+
+    
     
     func updateTimer(){
         totalSeconds-=1
         secondsToDisplay=totalSeconds%60
         minutesToDisplay=totalSeconds/60
         
-       
+        
         
         if secondsToDisplay<10{
-            focusTimeLabel.text="\(minutesToDisplay)" + ":" + "0"+"\(secondsToDisplay)"
+            logTimeLabel.text="\(minutesToDisplay)" + ":" + "0"+"\(secondsToDisplay)"
         }
         else{
-        focusTimeLabel.text="\(minutesToDisplay)"+":"+"\(secondsToDisplay)"
+            logTimeLabel.text="\(minutesToDisplay)"+":"+"\(secondsToDisplay)"
         }
         
         if totalSeconds==0{
             timer.invalidate()
             AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
-            self.performSegue(withIdentifier: "focusToLog", sender: self)
-        
+            
         }
         
         let toIncrementProgress:Float=1/(Float)(initialTime)
-        focusProgressBar.progress+=toIncrementProgress
+        logProgressBar.progress+=toIncrementProgress
         
         
     }
-
-override func viewDidLoad() {
-    super.viewDidLoad()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
     }
-
+    
     
     
 }
